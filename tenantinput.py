1af6acfe09202a81d6449c3ac600770f39196b29
a-torr3s <ashleytorres1122@gmail.com>
2023-04-12 09:51:59 -0700

    modified file

---
+++ tenantinput.py
@@ -0,0 +1,10 @@
+class tenantInput:
+
+    def __init__(self):
+        self.__aptNum = 0
+        self.__tenantName = None
+
+    def tenantInput(self):
+        aptNum = input("Apartment Number")
+    
+    
